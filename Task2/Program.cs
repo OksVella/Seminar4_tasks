730165af91dc11375e9d217c64dbfc6b1e5be70d
Ovella <oks.vella@yandex.ru>
2022-11-02 21:50:52 +0300

    added task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.Write("Enter a number: ");
+string num = Console.ReadLine();
+Console.WriteLine(calculate(num));
+
+int calculate(string num)
+{
+    int sum = 0;
+    for (int i = 0; i < num.Length; i++)
+    {
+        sum = sum + Convert.ToInt32(num[i].ToString());
+    }
+    return sum;
+}
