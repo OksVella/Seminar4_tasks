13ab5f8b555e56cc1b7ed881c4d66360b3b63982
Ovella <oks.vella@yandex.ru>
2022-11-02 19:35:38 +0300

    added task 1, added file gitignore

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите метод, который принимает на вход два числа (A и B) и возводит число A в целую степень B.
+int numA = new Random().Next(1, 10);
+int numB = new Random().Next(1, 10);
+
+Console.WriteLine("A = " + numA);
+Console.WriteLine("B = " + numB);
+Console.WriteLine(calculate(numA, numB));
+
+int calculate(int numA, int numB)
+{
+    int result = numA;
+    for (int i=1; i<numB; i++)
+    {
+        result = result * numA;
+    }
+
+    return result;
+}
