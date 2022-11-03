a4ddb306c9aa970b84ef54c33f795e7bb9805aa5
Ovella <oks.vella@yandex.ru>
2022-11-03 19:53:06 +0300

    added Task 3 and scheme

---
+++ Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+Console.Write("Enter array size: ");
+int[] array = new int[Convert.ToInt32(Console.ReadLine())];
+fillArray(array);
+printArray(array);
+
+void printArray(int[] array)
+{
+    for (int i=0; i<array.Length; i++)
+    {
+         Console.Write(array[i]);
+         if (i < array.Length-1)
+         {
+            Console.Write(", ");
+         }
+          
+    }
+}
+
+void fillArray(int[] array)
+{
+    for (int i=0; i<array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+    }
+}
